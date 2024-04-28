c2028309506bf34c33fbc5b2f517ed087a5c9ac8
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-04-28 21:35:59 +0200

    Support returning of multiple groups per cell
    This functionality is only supported on /api/plany.

--- src/getPlany.ts
+++ src/getPlany.ts
@@ -33,47 +33,61 @@ app.get("/api/plany/o:planId", async (req: Request, res: Response) => {
         planType.name = klasa
         let table: HTMLTableElement = document.querySelector('.tabela')!
         Array.from(table.rows).slice(1).forEach((rowElement, row_idx) => {
-            Array.from(rowElement.children).forEach((cell, idx, row) => {
-                if(idx >= 2 && cell.textContent?.trim()) {
-                    let tekst = cell.textContent
-
-                    let weekday = (idx - 2).toString() // 0..4
-                    let hour = parseInt(rowElement.querySelector('td.nr')?.textContent!)
-                    let godziny = rowElement.querySelector('td.g')?.textContent?.trim()!
-                    let lekcja = cell.querySelector('span.p')?.textContent!
-
-                    let arr_help = lekcja?.split('-')
-                    let grupa = "*"
-                    if(arr_help?.length! > 1) {
-                        grupa = arr_help[1].split('/')[0]
-                        lekcja = arr_help[0]
+            Array.from(rowElement.children).forEach((_cell, idx, row) => {
+                if(idx >= 2 && _cell.textContent?.trim()) {
+                    let to_process: Element[] = [];
+                    if(Array.from(_cell.children).some(el => el.tagName == 'BR')) {
+                        to_process = [document.createElement('td')];
+                        for(const node of Array.from(_cell.childNodes)) {
+                            if(node.nodeName == 'BR') {
+                                to_process.push(document.createElement('td'));
+                            } else {
+                                to_process[to_process.length-1].appendChild(node);
+                            }
+                        }
+                    } else {
+                        to_process = [_cell];
                     }
                     
+                    for(const cell of to_process) { // avoid overshadow
+                        let tekst = cell.textContent
 
-                    let nauczycielEl = cell.querySelector('a.n')
-                    let nauczycielSuf = nauczycielEl?.getAttribute('href')!;
-                    let nauczycielUrl = nauczycielSuf ? '/api/plany/' + nauczycielSuf : undefined;
-                    let nauczyciel = nauczycielEl?.textContent!
-
-                    let salaEl = cell.querySelector('a.s')
-                    let salaSuf = salaEl?.getAttribute('href')!;
-                    let salaUrl = salaSuf ? '/api/plany/' + salaSuf : undefined
-                    let sala = salaEl?.textContent!
-
-                    let lekcjaType: LekcjaType = { name: lekcja, godziny: godziny, sala: sala, salaUrl: salaUrl, nauczyciel: nauczyciel, nauczycielUrl: nauczycielUrl, grupa: grupa }
-                    if (!planType.plan[weekday]) {
-                        planType.plan[weekday] = {}
-                    }
-                    if(!planType.plan[weekday][hour]) {
-                        planType.plan[weekday][hour] = [lekcjaType];
-                    } else {
-                        planType.plan[weekday][hour].push(lekcjaType);
+                        let weekday = (idx - 2).toString() // 0..4
+                        let hour = parseInt(rowElement.querySelector('td.nr')?.textContent!)
+                        let godziny = rowElement.querySelector('td.g')?.textContent?.trim()!
+                        let lekcja = cell.querySelector('span.p')?.textContent!
+    
+                        let arr_help = lekcja?.split('-')
+                        let grupa = "*"
+                        if(arr_help?.length! > 1) {
+                            grupa = arr_help[1].split('/')[0]
+                            lekcja = arr_help[0]
+                        }
+                        
+    
+                        let nauczycielEl = cell.querySelector('a.n')
+                        let nauczycielSuf = nauczycielEl?.getAttribute('href')!;
+                        let nauczycielUrl = nauczycielSuf ? '/api/plany/' + nauczycielSuf : undefined;
+                        let nauczyciel = nauczycielEl?.textContent!
+    
+                        let salaEl = cell.querySelector('a.s')
+                        let salaSuf = salaEl?.getAttribute('href')!;
+                        let salaUrl = salaSuf ? '/api/plany/' + salaSuf : undefined
+                        let sala = salaEl?.textContent!
+    
+                        let lekcjaType: LekcjaType = { name: lekcja, godziny: godziny, sala: sala, salaUrl: salaUrl, nauczyciel: nauczyciel, nauczycielUrl: nauczycielUrl, grupa: grupa }
+                        if (!planType.plan[weekday]) {
+                            planType.plan[weekday] = {}
+                        }
+                        if(!planType.plan[weekday][hour]) {
+                            planType.plan[weekday][hour] = [lekcjaType]
+                        } else {
+                            planType.plan[weekday][hour].push(lekcjaType)
+                        }
                     }
                 }
             })
         })
-        
-        //planType.plan[klasa]
         return planType;
     })
     sendJSON(res, planType, 200)
